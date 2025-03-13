16cff9e4c26919140dbbb6fa42cd21da395ec95d
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2025-03-13 09:50:39 -0500

    Creación de DTO

--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { ProductosService } from './productos.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
 @Controller('productos')
+@UseInterceptors(BusinessErrorsInterceptor)
 export class ProductosController {
   constructor(private readonly productosService: ProductosService) {}
 }
